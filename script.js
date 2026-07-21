fa481fcfadc511871db087254aa2a200a7881d9b
asimfayyazexe <asimfayyaz109@gmail.com>
2026-07-21 11:52:58 +0500

    Add mouse circle effect and update styles for improved UI interaction

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector(".main"),
   smooth: true,
 });
+
+function mouseCircleFollow() {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector("#mouseCircle").style.transform =
+      `translate(${dets.clientX}px, ${dets.clientY}px)`;
+  });
+}
+
+mouseCircleFollow();
