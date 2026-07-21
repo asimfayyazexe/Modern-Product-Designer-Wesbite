8a69fefccfafc9dc5299d5ca4b621af3a83ad59d
asimfayyazexe <asimfayyaz109@gmail.com>
2026-07-21 13:36:32 +0500

    Added animations to hero-section nav, headings and footer using gsap

--- script.js
+++ script.js
@@ -3,6 +3,39 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
+function AnimateHeroPage() {
+  var t1 = gsap.timeline();
+
+  t1.from(".nav", {
+    y: 20,
+    opacity: 0,
+    duration: .5,
+    ease: Expo.ease
+  })
+    .to(".block-text", {
+      y: 0,
+      opacity: 1,
+      duration: .5,
+      ease: Expo.ease,
+      stagger: .5
+    }
+    )
+
+    .to(".block-text-2", {
+      y: 0,
+      opacity: 1,
+      duration: .5,
+      ease: Expo.ease,
+      stagger: .3
+    })
+    .from(".hero-footer", {
+      y: -10,
+      opacity: 0,
+      duration: 1,
+      ease: Expo.ease
+    })
+}
+
 function mouseCircleFollow() {
   window.addEventListener("mousemove", function (dets) {
     document.querySelector("#mouseCircle").style.transform =
@@ -11,3 +44,4 @@ function mouseCircleFollow() {
 }
 
 mouseCircleFollow();
+AnimateHeroPage()
